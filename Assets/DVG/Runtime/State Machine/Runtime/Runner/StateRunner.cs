37b7692ffc88a943ece480f8bc4a58d6987f696b
ducvg <doducvuong2003@gmail.com>
2026-05-08 08:15:50 +0700

    i can see it

--- Assets/DVG/Runtime/State Machine/Runtime/Runner/StateRunner.cs
+++ Assets/DVG/Runtime/State Machine/Runtime/Runner/StateRunner.cs
@@ -12,27 +12,90 @@ namespace DVG.StateMachine
         public void Unregister<TOwner>(State<TOwner> state) where TOwner : MonoBehaviour;
     }
     
-    internal abstract class StateRunner<TUpdate> : IStateRunner where TUpdate : class, IStateStatus
+    internal abstract class StateRunner<TUpdate> : IStateRunner where TUpdate : IStateStatus
     {
         private const int _initActiveSize = 128;
         
         protected TUpdate[] _states = new TUpdate[_initActiveSize];
         protected int _tailIndex;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void AddState(TUpdate state)
+        {
+            if (_states.Length == _tailIndex)
+            {
+                Array.Resize(ref _states, _tailIndex * 2);
+            }
+            _states[_tailIndex++] = state;
+        }
 
         public void Run()
         {
-            Span<TUpdate> stateSpan = _states.AsSpan(0, _tailIndex);
+            Span<TUpdate> stateSpan = _states.AsSpan();
             for (int i = 0; i < stateSpan.Length; ++i)
             {
                 var state = stateSpan[i];
-                TickState(state);
-                if (state.IsFinished)
+                if(state != null)
                 {
-                    stateSpan[i] = null;
+                    try
+                    {
+                        TickState(state);
+                        if (state.IsFinished) _states[i] = default;
+                        else continue;
+                    }
+                    catch (Exception e)
+                    {
+                        _states[i] = default;
+                        Debug.LogException(e);
+                    }
                 }
+
+                int j = _tailIndex - 1;
+                while (i < j)
+                {
+                    var fromTail = stateSpan[j];
+                    if (fromTail != null)
+                    {
+                        try
+                        {
+                            TickState(fromTail);
+                            if (fromTail.IsFinished)
+                            {
+                                stateSpan[j] = default;
+                                j--;
+                                continue; // next j
+                            }
+                            else
+                            {
+                                // swap
+                                stateSpan[i] = fromTail;
+                                stateSpan[j] = default;
+                                j--;
+                                goto NEXT_LOOP; // next i
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            stateSpan[j] = default;
+                            j--;
+                            Debug.LogException(ex);
+                            continue; // next j
+                        }
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+
+                _tailIndex = i; // loop end
+
+                NEXT_LOOP:
+                continue;
             }
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected abstract void TickState(TUpdate state);
 
         public bool Register<TOwner>(State<TOwner> state) where TOwner : MonoBehaviour
@@ -45,57 +108,53 @@ namespace DVG.StateMachine
 
         public void Unregister<TOwner>(State<TOwner> state) where TOwner : MonoBehaviour
         {
-            state.
+            state.IsFinished = true;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected void AddState(TUpdate state)
-        {
-            if (_states.Length == _tailIndex)
-            {
-                Array.Resize(ref _states, _tailIndex * 2);
-            }
-            _states[_tailIndex++] = state;
-        }
     }
     
     internal sealed class StateRunnerEarlyUpdate : StateRunner<IEarlyUpdate>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected override void TickState(IEarlyUpdate state)
         {
-            throw new NotImplementedException();
+            state.EarlyUpdate();
         }
     }
     
     internal sealed class StateRunnerFixedUpdate : StateRunner<IFixedUpdate>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected override void TickState(IFixedUpdate state)
         {
-            throw new NotImplementedException();
+            state.FixedUpdate();
         }
     }
     
     internal sealed class StateRunnerUpdate : StateRunner<IUpdate>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected override void TickState(IUpdate state)
         {
-            throw new NotImplementedException();
+            state.Update();
         }
     }
     
     internal sealed class StateRunnerPreLateUpdate : StateRunner<ILateUpdate>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected override void TickState(ILateUpdate state)
         {
-            throw new NotImplementedException();
+            state.LateUpdate();
         }
     }
     
     internal sealed class StateRunnerPostLateUpdate : StateRunner<IPostLateUpdate>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected override void TickState(IPostLateUpdate state)
         {
-            throw new NotImplementedException();
+            state.PostLateUpdate();
         }
     }
 }
--- Assets/DVG/Runtime/State Machine/Runtime/State/State.cs
+++ Assets/DVG/Runtime/State Machine/Runtime/State/State.cs
@@ -1,15 +1,22 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace DVG.StateMachine
 {
     public interface IStateStatus
     {
-        internal bool IsFinished { get; set; }
+        public bool IsFinished { get; }
     }
     
     public abstract class State<TOwner> : IStateStatus where TOwner : MonoBehaviour
     {
-        internal bool IStateStatus.IsFinished { get; set; }
+        public bool IsFinished 
+        { 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get; 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal set; 
+        }
         
         public abstract void OnEnter(TOwner owner);
         public abstract void OnExit(TOwner owner);
