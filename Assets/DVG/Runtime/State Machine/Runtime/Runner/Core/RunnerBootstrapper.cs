30195e596684fe3f22c15781dab5c8e3a02390d8
ducvg <doducvuong2003@gmail.com>
2026-06-19 19:03:43 +0700

    lost update

--- Assets/DVG/Runtime/State Machine/Runtime/Runner/Core/RunnerBootstrapper.cs
+++ Assets/DVG/Runtime/State Machine/Runtime/Runner/Core/RunnerBootstrapper.cs
@@ -8,152 +8,148 @@ using UnityEngine.PlayerLoop;
 
 namespace DVG.StateMachine
 {
-    internal static class RunnerBootstrapper
-    {
-        internal static void Initialize() 
-        {
-            PlayerLoopSystem currentPlayerLoop = 
-#if UNITY_2019_3_OR_NEWER
-                PlayerLoop.GetCurrentPlayerLoop();
-#else
-                PlayerLoop.GetDefaultPlayerLoop();
-#endif
-            var earlyUpdateRunner = new StateRunnerEarlyUpdate();
-            var fixedUpdateRunner = new StateRunnerFixedUpdate();
-            var updateRunner = new StateRunnerUpdate();
-            var preLateUpdateRunner = new StateRunnerPreLateUpdate();
-            var postLateUpdateRunner = new StateRunnerPostLateUpdate();
+	internal static class RunnerBootstrapper
+	{
+		internal static void Initialize()
+		{
+			var earlyUpdateRunner = new StateRunnerEarlyUpdate();
+			var fixedUpdateRunner = new StateRunnerFixedUpdate();
+			var updateRunner = new StateRunnerUpdate();
+			var preLateUpdateRunner = new StateRunnerPreLateUpdate();
+			var postLateUpdateRunner = new StateRunnerPostLateUpdate();
 
-            PlayerLoopSystem earlyUpdateLoop = new()
-            {
-                type = typeof(StateRunnerEarlyUpdate),
-                updateDelegate = () => earlyUpdateRunner.Run(Time.deltaTime)
-            };
-            PlayerLoopSystem fixedUpdateLoop = new()
-            {
-                type = typeof(StateRunnerFixedUpdate),
-                updateDelegate = () => fixedUpdateRunner.Run(Time.fixedDeltaTime)
-            };
-            PlayerLoopSystem updateLoop = new()
-            {
-                type = typeof(StateRunnerUpdate),
-                updateDelegate = () => updateRunner.Run(Time.deltaTime)
-            };
-            PlayerLoopSystem preLateUpdateLoop = new()
-            {
-                type = typeof(StateRunnerPreLateUpdate),
-                updateDelegate = () => preLateUpdateRunner.Run(Time.deltaTime)
-            };
-            PlayerLoopSystem postLateUpdateLoop = new()
-            {
-                type = typeof(StateRunnerPostLateUpdate),
-                updateDelegate = () => postLateUpdateRunner.Run(Time.deltaTime)
-            };
+			StateManager.SetRunners(earlyUpdateRunner, fixedUpdateRunner, updateRunner, preLateUpdateRunner, postLateUpdateRunner);
 
-            StateManager.SetRunners(earlyUpdateRunner, fixedUpdateRunner, updateRunner, preLateUpdateRunner, postLateUpdateRunner);
+			PlayerLoopSystem earlyUpdateLoop = new()
+			{
+				type = typeof(StateRunnerEarlyUpdate),
+				updateDelegate = () => earlyUpdateRunner.Run(Time.deltaTime)
+			};
+			PlayerLoopSystem fixedUpdateLoop = new()
+			{
+				type = typeof(StateRunnerFixedUpdate),
+				updateDelegate = () => fixedUpdateRunner.Run(Time.fixedDeltaTime)
+			};
+			PlayerLoopSystem updateLoop = new()
+			{
+				type = typeof(StateRunnerUpdate),
+				updateDelegate = () => updateRunner.Run(Time.deltaTime)
+			};
+			PlayerLoopSystem preLateUpdateLoop = new()
+			{
+				type = typeof(StateRunnerPreLateUpdate),
+				updateDelegate = () => preLateUpdateRunner.Run(Time.deltaTime)
+			};
+			PlayerLoopSystem postLateUpdateLoop = new()
+			{
+				type = typeof(StateRunnerPostLateUpdate),
+				updateDelegate = () => postLateUpdateRunner.Run(Time.deltaTime)
+			};
 
-            InsertSystems(ref currentPlayerLoop,
-                earlyUpdateLoop: earlyUpdateLoop,
-                fixedUpdateLoop: fixedUpdateLoop,
-                updateLoop: updateLoop,
-                preLateUpdateLoop: preLateUpdateLoop,
-                postLateUpdateLoop: postLateUpdateLoop
-            );
+			PlayerLoopSystem currentPlayerLoop = PlayerLoop.GetCurrentPlayerLoop();
 
-            PlayerLoop.SetPlayerLoop(currentPlayerLoop);
-            
-#if UNITY_EDITOR
-            EditorApplication.playModeStateChanged += OnPlaymodeExit;
-#endif
-        }
+			InsertSystems(ref currentPlayerLoop,
+				earlyUpdateLoop: earlyUpdateLoop,
+				fixedUpdateLoop: fixedUpdateLoop,
+				updateLoop: updateLoop,
+				preLateUpdateLoop: preLateUpdateLoop,
+				postLateUpdateLoop: postLateUpdateLoop
+			);
 
-        private static void InsertSystems(ref PlayerLoopSystem rootSystem,
-            in PlayerLoopSystem earlyUpdateLoop,
-            in PlayerLoopSystem fixedUpdateLoop,
-            in PlayerLoopSystem updateLoop,
-            in PlayerLoopSystem preLateUpdateLoop,
-            in PlayerLoopSystem postLateUpdateLoop)
-        {
-            const int insertIndex = 0;
-            
-            Span<PlayerLoopSystem> rootSpan = rootSystem.subSystemList;
-
-            for (int i = 0; i < rootSpan.Length; i++)
-            {
-                ref PlayerLoopSystem subSystem = ref rootSpan[i];
-
-                PlayerLoopSystem insertSystem;
-
-                Type type = subSystem.type;
-
-                if (type == typeof(EarlyUpdate)) insertSystem = earlyUpdateLoop;
-                else if (type == typeof(FixedUpdate)) insertSystem = fixedUpdateLoop;
-                else if (type == typeof(Update)) insertSystem = updateLoop;
-                else if (type == typeof(PreLateUpdate)) insertSystem = preLateUpdateLoop;
-                else if (type == typeof(PostLateUpdate)) insertSystem = postLateUpdateLoop;
-                else continue;
-
-                PlayerLoopSystem[] oldList = subSystem.subSystemList;
-
-                int oldLength = oldList?.Length ?? 0;
-
-                PlayerLoopSystem[] newList = new PlayerLoopSystem[oldLength + 1];
-
-                newList[insertIndex] = insertSystem;
-
-                if (oldLength > 0) Array.Copy(oldList, 0, newList, insertIndex + 1, oldLength);
-
-                subSystem.subSystemList = newList;
-            }
-        }
+			PlayerLoop.SetPlayerLoop(currentPlayerLoop);
 
 #if UNITY_EDITOR
-        private static void OnPlaymodeExit(PlayModeStateChange playModeState)
-        {
-            if (playModeState == PlayModeStateChange.ExitingPlayMode)
-            {
-                var currentPlayerLoop = PlayerLoop.GetCurrentPlayerLoop();
-                
-                currentPlayerLoop.RemoveSystemFrom<EarlyUpdate>(typeof(StateRunnerEarlyUpdate));
-                currentPlayerLoop.RemoveSystemFrom<FixedUpdate>(typeof(StateRunnerFixedUpdate));
-                currentPlayerLoop.RemoveSystemFrom<Update>(typeof(StateRunnerUpdate));
-                currentPlayerLoop.RemoveSystemFrom<PreLateUpdate>(typeof(StateRunnerPreLateUpdate));
-                currentPlayerLoop.RemoveSystemFrom<PostLateUpdate>(typeof(StateRunnerPostLateUpdate));
-                
-                PlayerLoop.SetPlayerLoop(currentPlayerLoop);
-
-                EditorApplication.playModeStateChanged -= OnPlaymodeExit;
-            }
-        }
-        
-        private static void RemoveSystemFrom<TLoop>(this ref PlayerLoopSystem rootSystem, Type systemToRemove)
-        {
-            PlayerLoopSystem[] rootSubSystems = rootSystem.subSystemList;
-
-            if (rootSubSystems != null)
-            {
-                int subSystemCount = rootSubSystems.Length;
-                List<PlayerLoopSystem> newSubSystemList = new(subSystemCount);
-
-                Span<PlayerLoopSystem> allSubSystemsSpan = rootSubSystems.AsSpan();
-
-                for (int i = 0; i < subSystemCount; ++i)
-                {
-                    PlayerLoopSystem subSystem = allSubSystemsSpan[i];
-
-                    if (subSystem.type == typeof(TLoop))
-                    {
-                        subSystem.subSystemList = subSystem.subSystemList?
-                            .Where(s => s.type != systemToRemove)
-                            .ToArray();
-                    }
-
-                    newSubSystemList.Add(subSystem);
-                }
-
-                rootSystem.subSystemList = newSubSystemList.ToArray();
-            }
-        }
+			EditorApplication.playModeStateChanged += OnPlaymodeExit;
 #endif
-    }
+		}
+
+		private static void InsertSystems(ref PlayerLoopSystem rootSystem,
+			in PlayerLoopSystem earlyUpdateLoop,
+			in PlayerLoopSystem fixedUpdateLoop,
+			in PlayerLoopSystem updateLoop,
+			in PlayerLoopSystem preLateUpdateLoop,
+			in PlayerLoopSystem postLateUpdateLoop)
+		{
+			const int insertIndex = 0;
+
+			Span<PlayerLoopSystem> rootSpan = rootSystem.subSystemList;
+
+			for (int i = 0; i < rootSpan.Length; i++)
+			{
+				ref PlayerLoopSystem subSystem = ref rootSpan[i];
+
+				PlayerLoopSystem insertSystem;
+
+				Type type = subSystem.type;
+
+				if (type == typeof(EarlyUpdate)) insertSystem = earlyUpdateLoop;
+				else if (type == typeof(FixedUpdate)) insertSystem = fixedUpdateLoop;
+				else if (type == typeof(Update)) insertSystem = updateLoop;
+				else if (type == typeof(PreLateUpdate)) insertSystem = preLateUpdateLoop;
+				else if (type == typeof(PostLateUpdate)) insertSystem = postLateUpdateLoop;
+				else continue;
+
+				PlayerLoopSystem[] oldList = subSystem.subSystemList;
+
+				int oldLength = oldList?.Length ?? 0;
+
+				PlayerLoopSystem[] newList = new PlayerLoopSystem[oldLength + 1];
+
+				newList[insertIndex] = insertSystem;
+
+				if (oldLength > 0) Array.Copy(oldList, 0, newList, insertIndex + 1, oldLength);
+
+				subSystem.subSystemList = newList;
+			}
+		}
+
+#if UNITY_EDITOR
+		private static void OnPlaymodeExit(PlayModeStateChange playModeState)
+		{
+			if (playModeState == PlayModeStateChange.ExitingPlayMode)
+			{
+				var currentPlayerLoop = PlayerLoop.GetCurrentPlayerLoop();
+
+				currentPlayerLoop.RemoveSystemFrom<EarlyUpdate>(typeof(StateRunnerEarlyUpdate));
+				currentPlayerLoop.RemoveSystemFrom<FixedUpdate>(typeof(StateRunnerFixedUpdate));
+				currentPlayerLoop.RemoveSystemFrom<Update>(typeof(StateRunnerUpdate));
+				currentPlayerLoop.RemoveSystemFrom<PreLateUpdate>(typeof(StateRunnerPreLateUpdate));
+				currentPlayerLoop.RemoveSystemFrom<PostLateUpdate>(typeof(StateRunnerPostLateUpdate));
+
+				PlayerLoop.SetPlayerLoop(currentPlayerLoop);
+
+				EditorApplication.playModeStateChanged -= OnPlaymodeExit;
+			}
+		}
+
+		private static void RemoveSystemFrom<TLoop>(this ref PlayerLoopSystem rootSystem, Type systemToRemove)
+		{
+			PlayerLoopSystem[] rootSubSystems = rootSystem.subSystemList;
+
+			if (rootSubSystems != null)
+			{
+				int subSystemCount = rootSubSystems.Length;
+				List<PlayerLoopSystem> newSubSystemList = new(subSystemCount);
+
+				Span<PlayerLoopSystem> allSubSystemsSpan = rootSubSystems.AsSpan();
+
+				for (int i = 0; i < subSystemCount; ++i)
+				{
+					PlayerLoopSystem subSystem = allSubSystemsSpan[i];
+
+					if (subSystem.type == typeof(TLoop))
+					{
+						subSystem.subSystemList = subSystem.subSystemList?
+							.Where(s => s.type != systemToRemove)
+							.ToArray();
+					}
+
+					newSubSystemList.Add(subSystem);
+				}
+
+				rootSystem.subSystemList = newSubSystemList.ToArray();
+			}
+		}
+#endif
+	}
 }
