ff0bcc0873274d5cdca2af225aff0eefd7db669d
ducvg <doducvuong2003@gmail.com>
2026-06-19 19:03:55 +0700

    improve scriptable singleton

--- Assets/~Common Scripts~/Singleton.cs
+++ Assets/~Common Scripts~/Singleton.cs
@@ -1,5 +1,6 @@
+using System;
+using UnityEditor;
 using UnityEngine;
-
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
 	private static T _instance;
@@ -36,5 +37,59 @@ public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehavi
 		base.Awake();
 		transform.SetParent(null);
 		DontDestroyOnLoad(gameObject);
+	}
+}
+
+public abstract class ScriptableSingleton<T> : ScriptableSingleton where T : ScriptableObject
+{
+	public static T Instance => _instance;
+	private static T _instance;
+
+	protected void Awake()
+	{
+		Debug.Log("awake");
+		if (_instance != null && _instance != this) return;
+		_instance = this as T;
+	}
+}
+
+public abstract class ScriptableSingleton : ScriptableObject
+{
+	[InitializeOnLoadMethod]
+	private static void LoadPreloadedAssets() => PlayerSettings.GetPreloadedAssets();
+
+	private class ScriptableSingletonImporter : AssetPostprocessor
+	{
+		private static void OnPostprocessAllAssets(
+			string[] importedAssets,
+			string[] deletedAssets,
+			string[] movedAssets,
+			string[] movedFromAssetPaths,
+			bool didDomainReload)
+		{
+			bool isDeleteting = deletedAssets.Length > 0;
+			bool isMoving = movedAssets.Length > 0 || movedFromAssetPaths.Length > 0;
+			if (didDomainReload || isDeleteting || isMoving) return;
+
+			string createdPath = importedAssets[0];
+			var singleton = AssetDatabase.LoadAssetAtPath<ScriptableSingleton>(createdPath);
+			if (!singleton) return;
+
+			//preload assets preload in runtime, not in editor startup, dumbshit
+			var preloadAssetArray = PlayerSettings.GetPreloadedAssets();
+			if (preloadAssetArray == null) return;
+			foreach (var preloaded in preloadAssetArray)
+			{
+				if (preloaded.GetType() == singleton.GetType())
+				{
+					AssetDatabase.DeleteAsset(createdPath);
+					EditorGUIUtility.PingObject(preloaded);
+					return;
+				}
+			}
+			Array.Resize(ref preloadAssetArray, preloadAssetArray.Length + 1);
+			preloadAssetArray[^1] = singleton;
+			PlayerSettings.SetPreloadedAssets(preloadAssetArray);
+		}
 	}
 }
